0796473144bc03eb97d61f69c5c004d8e86a67b5
Andreas Rozek <A.Rozek@gmx.de>
2022-08-28 06:08:22 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,16 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: './src/aframe-axeshelper.ts',
+  input: './src/aframe-axeshelper-component.ts',
   output: [
     {
-      file:     './dist/aframe-axeshelper.js',
+      file:     './dist/aframe-axeshelper-component.js',
       format:    'iife', // module is loaded for side-effects only
       noConflict:true,
       sourcemap: true,
       plugins: [terser({ format:{ comments:false, safari10:true } })],
     },{
-      file:     './dist/aframe-axeshelper.esm.js',
+      file:     './dist/aframe-axeshelper-component.esm.js',
       format:   'esm',
       sourcemap:true,
     }
